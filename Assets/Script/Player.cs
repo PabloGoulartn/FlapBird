1ccfc0cfaed3db7744aa99579784385a79ef0119
Pablo Goulart <pablogoulart02@gmail.com>
2025-04-03 18:17:54 -0300

    Player inclina ao cair

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    [SerializeField] private float rotate;
+    [SerializeField] private short speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            if (rotate > -45f)
+            {
+                rotate = Mathf.Max(-45f, rotate - Time.deltaTime * speed);
+                transform.Rotate(0, 0, -Time.deltaTime * speed);
+            }
+        }
+    }
+}
